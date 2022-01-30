d38fa7ff2a75c9ff74ad8e7257b7454bd0a2a685
iftikharbalouch744 <iftikharbalouch744@gmail.com>
2022-01-30 23:54:32 +0500

    signup lable change

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="card login">
 <div class="card header">
-<h4>Sign Up New User </h4>
+<h4>Sign Up New User</h4>
 </div>
 <form action="/signup" method="POST">
     <br/>
